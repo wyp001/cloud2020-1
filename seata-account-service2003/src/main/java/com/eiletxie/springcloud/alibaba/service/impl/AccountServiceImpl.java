fd51bf3c1239b8a516885d187c4c66904a67af58
wyupeng <1834583115@qq.com>
2022-04-25 14:15:48 +0800

    seata-0.9.0测试

--- seata-account-service2003/src/main/java/com/eiletxie/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ seata-account-service2003/src/main/java/com/eiletxie/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -26,11 +26,11 @@ public class AccountServiceImpl implements AccountService {
     public void decrease(Long userId, BigDecimal money) {
         LOGGER.info("----> account-service中扣减用户余额开始");
 
-        try {
-            TimeUnit.SECONDS.sleep(20);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // try {
+        //     TimeUnit.SECONDS.sleep(20);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
         accountDao.decrease(userId,money);
         LOGGER.info("----> account-service中扣减用户余额开始");
     }
